59120ccd6230cf73bb99fd70fea8bdceb0f7d92f
bingyi-ad <bingyizhu@aldagram.com>
2024-01-09 14:23:56 +0900

    init commit

---
+++ RenovateTest/RenovateTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RenovateTest
+//
+//  Created by 朱　冰一 on 2024/01/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RenovateTest/RenovateTest/RenovateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenovateTestApp.swift
+//  RenovateTest
+//
+//  Created by 朱　冰一 on 2024/01/09.
+//
+
+import SwiftUI
+
+@main
+struct RenovateTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
